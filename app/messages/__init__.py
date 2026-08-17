b5679eec63419b1c48dcb9971b1207201bb6fbfd
YourName <alifbahrami13766@gmail.com>
2026-08-17 08:36:49 +0330

    feat: add and complete task messages

---
+++ app/messages/__init__.py
@@ -0,0 +1,3 @@
+
+from .task import Messages as TaskMessages
+from .auth import Messages as AuthMessages
---
+++ app/messages/task.py
@@ -0,0 +1,7 @@
+
+class Messages:
+    TASK_CREATION_FAILED = "Failed to create task."
+    TASK_FETCHING_FAILED = "Failed to fetch task."
+    TASK_UPDATE_FAILED = "Failed to update task."
+    TASK_DELETION_FAILED = "Failed to delete task."
+    TASK_NOT_FOUND = "Task not found."
